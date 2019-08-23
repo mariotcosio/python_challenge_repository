70e15de96f7e22f69ffdfc0abfc492c39148acb0
Mario <53076792+mariotcosio@users.noreply.github.com>
2019-08-23 17:00:18 -0500

    finishe

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,52 +1,53 @@
 # Import modules
 import os 
 import csv
-# Write a function that does:
-# names a variable as csv
-def gethestuff(csv):
-    # defining variables
-    months=0
-    total=0
-    maxrev=0
-    minrev=0
-    avgchange=0
-    # "" indicates a string
-    maxmonth = ""
-    minmonth = ""
-    # row is located in csv, looping through row
-    for row in csv:
-        current_month = row[0]
-        pnl = row[1]
-        total += pnl
-        #Number of months
-        months += 1
-       # if something is > something else:
-            #do something
-        # this makes the highest amount of revenue as the max while it goes through the loop
-        if pnl > maxrevenue:
-            maxrevenue = pnl
-            maxmonth = current_month
-    return [months, total, maxrev, minrev, avgchange]
-
- 
-
-# Greatest profit and month
-
-
-# Greatest Loss and month
-# Average Change
-# Greatest Month
-#set input file path
-#Open the file
-# Read Source file
-with open(path) as file:
-    csvreader = csv.reader(file, delimiter = ',')
+# Set file path for csv file
+with open(budget_data.csv) as budget_data_file:
+    csvreader = csv.reader(budget_data_file, delimiter = ',')
     header = next(csvreader)
-    analysis = getthestuff(csvreader)
-print(analysis[0])
+# defining variables
+    total_months = 0
+    total_rev = 0
+    rev = []
+    pre_rev = 0
+    month_change = 0
+    rev_change = 0
+    greatest_increase = ["",0]
+    greatest_decrease = ["",99999999]
+    rev_change_list = []
+    avg_rev = 0
+#Total number of months and total revenue
+    for row in csvreader:
+        total_months += 1
+        total_rev += int(row["Profit/Losses"])
+#Average change in profits
+        avgchange = int(row["Profit/Losses"]) - prevrev
+        prevrev = int(row["Profit/Losses"])
+        rev_change_list += [rev_change]
+        month_change += [row["Date"]]
+        # if something is > something else:
+        if rev_change > greatest_increase[1]:
+            greatest_increase[1] = rev_change
+            greatest_increase[0] = row["Date"]
+        if rev_change < greatest_decrease[1]:
+            greatest_decrease[1] = rev_change
+            greatest_decrease[0] = row["Date"]
+    avg_rev = sum(rev_change_list)/len(rev_change_list)
 
 # set output file path
-# Print answer    
-# print(total) will show total revenue
-print(total)
+
+# Print answer
+print("Financial Analysis"")
+print("Total Months: " + str(total_months))
+print("Total Revenue: " + $ + str(total_rev))
+print("Average Change: " + $ + str(avg_rev))
+print("Greatest Increase: " + str(greatest_increase[0]) + " ($" + str(greatest_increase[1]) +")")
+print("Greatest Decrease: " + str(greatest_decrease[0]) + " ($" + str(greatest_decrease[1]) +")")
 # Save to text file
+with open(text, "w") as text_file
+    text_file.write("Total Months: " + str(total_months))
+    text_file.write("Total Revenue: " + "$" + str(total_rev))
+    text_file.write("Average Change: " + "$" + str(avg_rev))
+    text_file.write("Greatest Increase: " + str(greatest_increase[0]) + " ($" + str(greatest_increase[1]) +")")
+    text_file.write("Greatest Decrease: " + str(greatest_decrease[0]) + " ($" + str(greatest_decrease[1]) +")")
+
